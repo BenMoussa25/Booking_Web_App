e0864344196297397c8467d9b0151402f069faf8
Mohamed Aziz Ben Moussa <58788007+BenMoussa25@users.noreply.github.com>
2025-05-13 12:07:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Example with Gmail
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cyberspherecongress@gmail.com'  # Your email
-app.config['MAIL_PASSWORD'] = 'ylvu rngx qwnr awwq'  # Your email password or app password
+app.config['MAIL_PASSWORD'] = 'password'  # Your email password or app password
 app.config['MAIL_DEFAULT_SENDER'] = 'cyberspherecongress@gmail.com'
 mail = Mail(app)
 
